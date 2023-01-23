0eb395f1149f00dd5057dd9f1acb848ed8be2751
Atcheri Atch <atcheri@gmail.com>
2023-01-24 00:52:41 +0100

    hajime no commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
+  import ProductItem from "./lib/ProductItem.svelte";
 </script>
 
-<main>
+<main class="h-screen w-screen bg-indigo-700">
   <h1>Vite + Svelte + TS + Tailwindcss</h1>
   <h2>Starter kit</h2>
+  <div class="flex justify-center items-center">
+    <ProductItem />
+  </div>
 </main>
 
 <style>
---
+++ src/lib/ProductItem.svelte
@@ -0,0 +1,96 @@
+<script lang="ts">
+  import FaCartPlus from "svelte-icons/fa/FaCartPlus.svelte";
+  import FaRegEye from "svelte-icons/fa/FaRegEye.svelte";
+  import FaHeart from "svelte-icons/fa/FaHeart.svelte";
+  import FaStar from "svelte-icons/fa/FaStar.svelte";
+  import type { Product } from "./types";
+
+  const product: Product = {
+    id: 6,
+    title: "MacBook Pro",
+    description:
+      "MacBook Pro 2021 with mini-LED display may launch between September, November",
+    price: 1749,
+    discountPercentage: 11.02,
+    rating: 4.57,
+    stock: 83,
+    brand: "APPle",
+    category: "laptops",
+    thumbnail: "https://i.dummyjson.com/data/products/6/thumbnail.png",
+    images: [
+      "https://i.dummyjson.com/data/products/6/1.png",
+      "https://i.dummyjson.com/data/products/6/2.jpg",
+      "https://i.dummyjson.com/data/products/6/3.png",
+      "https://i.dummyjson.com/data/products/6/4.jpg",
+    ],
+  };
+</script>
+
+<div class="w-96">
+  <div class="relative w-full h-80 overflow-hidden">
+    <img
+      class="top-0 left-0 w-full h-full object-cover hover:scale-150 ease-in-out duration-500 origin-right"
+      src={product.images[0]}
+      alt={product.title}
+    />
+    <ul class="action absolute top-3 right-3">
+      <li
+        class="group flex justify-center items-center m-1 relative list-none bg-white w-10 h-10 p-3 cursor-pointer hover:bg-red-500 hover:text-slate-50 hover:opacity-100"
+      >
+        <FaCartPlus />
+        <span
+          class="opacity-0 absolute right-[50px] bg-white text-gray-900 p-2 whitespace-nowrap pointer-events-none group-hover:opacity-100"
+          >Add to cart</span
+        >
+      </li>
+      <li
+        class="group flex justify-center items-center m-1 relative list-none bg-white w-10 h-10 p-3 cursor-pointer hover:bg-red-500 hover:text-slate-50 hover:opacity-100"
+      >
+        <FaRegEye />
+        <span
+          class="opacity-0 absolute right-[50px] bg-white text-gray-900 p-2 whitespace-nowrap pointer-events-none group-hover:opacity-100"
+          >View details</span
+        >
+      </li>
+      <li
+        class="group flex justify-center items-center m-1 relative list-none bg-white w-10 h-10 p-3 cursor-pointer hover:bg-red-500 hover:text-slate-50 hover:opacity-100"
+      >
+        <FaHeart />
+        <span
+          class="opacity-0 absolute right-[50px] bg-white text-gray-900 p-2 whitespace-nowrap pointer-events-none group-hover:opacity-100"
+          >Add to widh list</span
+        >
+      </li>
+    </ul>
+  </div>
+  <div class="content">
+    <h2 class="text-lg font-semibold text-slate-800 mt-2">
+      {product.title} <br />
+      <span class="" />
+    </h2>
+    <p class="" />
+    <div class="price-rating flex justify-between items-center">
+      <h2 class="text-red-600 text-xl font-semibold">{product.price}$</h2>
+      <div class="rating flex flex-wrap">
+        <span class="h-5 text-yellow-400 cursor-pointer">
+          <FaStar />
+        </span>
+        <span class="h-5 text-yellow-400 cursor-pointer">
+          <FaStar />
+        </span>
+        <span class="h-5 text-yellow-400 cursor-pointer">
+          <FaStar />
+        </span>
+        <span class="h-5 text-yellow-400 cursor-pointer">
+          <FaStar />
+        </span>
+        <span class="h-5 text-yellow-400 cursor-pointer">
+          <FaStar />
+        </span>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+</style>
